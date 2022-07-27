6f7852494c8f4ea98b9c2a44de172acd547a351d
Bryce Curtis <curtis.bryce@gmail.com>
2022-07-27 09:59:51 -0400

    Update for Android

--- track/htc/mobile/src/android/app/src/main/java/com/msdkproject/MainActivity.java
+++ track/htc/mobile/src/android/app/src/main/java/com/msdkproject/MainActivity.java
@@ -1,8 +1,22 @@
 package com.msdkproject;
 
 import com.facebook.react.ReactActivity;
+import android.content.ContextWrapper;
+import android.system.Os;
+
 
 public class MainActivity extends ReactActivity {
+
+@Override
+protected void onStart() {
+    super.onStart();
+    try {
+        ContextWrapper c = new ContextWrapper(this);
+        Os.setenv("EXTERNAL_STORAGE", c.getFilesDir().toString(), true);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
