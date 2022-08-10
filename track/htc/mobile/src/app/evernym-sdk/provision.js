adf64e2a0a10a67a39414be29aca59668539bccb
nmoskaleva <natalia.moskaleva@evernym.com>
2022-08-09 21:01:02 -0700

    add mobile sponsor registration instructions

--- track/htc/mobile/src/app/evernym-sdk/provision.js
+++ track/htc/mobile/src/app/evernym-sdk/provision.js
@@ -19,5 +19,28 @@ export const DEFAULT_SERVER_ENVIRONMENT = "DEMO"
  * */
 export const GET_PROVISION_TOKEN_FUNC = null
 
+/* An example of a GET_PROVISION_TOKEN_FUNC function. Make sure to put your actual Sponsor's Endpoint. 
+
+export const GET_PROVISION_TOKEN_FUNC = async () => {
+  try {
+     const response = await fetch('https://2b61-64-62-226-18.eu.ngrok.io/generate', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({"sponseeId": "545516d9-9c5d-4bae-84c6-a74989499cc5"}),
+     });
+const json = await response.json();
+console.log(JSON.stringify(json));
+return [null, JSON.stringify(json)]
+}
+     catch (error) {
+     return [error.message, null]
+  }
+}
+
+*/
+
 export const VCX_PUSH_TYPE = 3
 
